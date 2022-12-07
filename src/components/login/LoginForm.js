a0aa0fade6bc3f5b94960b8a8fe9554d0593160a
Presha Sunar <preshasuna@gmail.com>
2022-12-07 21:22:05 +0545

    reset page made

--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -1,19 +1,22 @@
-import React from 'react'
+import React from 'react';
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import  PropTypes  from 'prop-types';
 
-const LoginForm = () => {
+const LoginForm = ({handleOnChange, handleOnSubmit, formSwitcher, email, password}) => {
   return (
     <Container>
         <Row>
             <Col>
-            <h1 className="text-info text-center">Client Login</h1>  
+            <h1 className="text-info text-center">Login</h1>  
             <hr />
-            <Form>
+            <Form autoComplete ="off" onSubmit={handleOnSubmit}>
                 <Form.Group>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
                     type="email"
                     name="email"
+                    value = {email}
+                    onChange={handleOnChange}
                     placeholder="Enter Email"
                     required
                     />
@@ -24,23 +27,30 @@ const LoginForm = () => {
                     <Form.Control
                         type="password"
                         name="password"
+                        value = {password}
+                        onChange = {handleOnChange}
                         placeholder="password"
                         required
                     />
                 </Form.Group>
-                <Button type = "submit">Login</Button>
+                <Button type = "submit">Customer Login</Button>
             </Form> 
             <hr/>
             </Col>
         </Row>
         <Row>
             <Col>
-            <a href="#!">Forgot Password?</a>
+            <a href="#!" onClick={() => formSwitcher('reset')}>Forgot Password?</a>
             </Col>
         </Row>
       
     </Container>
   );
 }
-
+LoginForm.propTypes = {
+    handleOnChange: PropTypes.func.isRequired, 
+    handleOnSubmit: PropTypes.func.isRequired, 
+    formSwitcher: PropTypes.func.isRequired,
+    email: PropTypes.string.isRequired, 
+    password: PropTypes.string.isRequired }
 export default LoginForm;
--- src/pages/entry/Entry.js
+++ src/pages/entry/Entry.js
@@ -1,13 +1,81 @@
-import React from "react";
+import React, {useState} from "react";
 import {Jumbotron} from "react-bootstrap"
 import LoginForm  from "../../components/login/LoginForm";
+import ResetForm  from "../../components/password-reset/ResetForm";
 import "./entry.css";
 
+
 const Entry = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [formLoad, setFormLoad] = useState("login");
+
+const handleOnChange = (e) =>{
+    const {name, value} = e.target
+
+    switch(name){
+      case 'email':
+        setEmail(value);
+        break;
+      case 'password':
+          setPassword(value);
+          break;
+        
+        default:
+          break;
+    }
+  };
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+
+    if(!email || !password)
+    {
+      return alert("Fill up all the field!");
+    }
+    // call API to submit the form
+    console.log(email, password);
+  };
+  
+
+  const handleOnResetSubmit = (e) => {
+    e.preventDefault();
+
+    if(!email)
+    {
+      return alert("Please enter the Email!");
+    }
+    // call API to submit the form
+    console.log(email);
+  };
+  const formSwitcher = (formType) => {
+    setFormLoad(formType);
+  };
+
+  
+
   return (
     <div className="Entry-page bg-info">
-      <Jumbotron >
-        <LoginForm/>
+      <Jumbotron className="form-box">
+        {formLoad === 'login' && 
+        <LoginForm 
+        handleOnChange = {handleOnChange}
+        handleOnSubmit = {handleOnSubmit}
+        formSwitcher = {formSwitcher}
+        email = {email}
+        password = {password}
+        />}
+        
+        
+
+        {formLoad === 'reset' && 
+        <ResetForm 
+        handleOnChange = {handleOnChange}
+        handleOnResetSubmit = {handleOnResetSubmit}
+        formSwitcher = {formSwitcher}
+        email = {email}
+        />}
+
       </Jumbotron>
     </div>
   );
